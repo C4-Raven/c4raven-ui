78a9c6379c2da505529f2a9bc0f5f72e0011520a
Josh <josh@c4raven.net>
2026-09-01 11:29:10 +1000

    Update generated version stamp

--- src/_versions.ts
+++ src/_versions.ts
@@ -11,8 +11,8 @@ export interface TsAppVersion {
 export const versions: TsAppVersion = {
     version: '0.0.0',
     name: 'opentakserver-ui',
-    versionDate: '2026-08-30T05:38:32.970Z',
-    gitCommitHash: '35f51f6',
-    versionLong: '0.0.0-35f51f6',
+    versionDate: '2026-09-01T01:29:04.793Z',
+    gitCommitHash: 'cb5ef93',
+    versionLong: '0.0.0-cb5ef93',
 };
 export default versions;
